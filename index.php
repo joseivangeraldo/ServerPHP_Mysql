362403403871f9a601eac203e60238fbd9367568
José Ivan Geraldo da Silva <67489053+joseivangeraldo@users.noreply.github.com>
2023-04-24 11:26:50 +0000

    YOUR_COMMIT_MESSAGE

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$connect = mysqli_connect('db','lamp_demo','password','lamp_demo')
+    or die ('Não foi possivel conectar');
+
+$query = 'SELECT * FROM blog';
+$result = mysqli_query($connect, $query);
+
+echo '<h1>MySQL Content:</h1>';
+
+while($record = mysqli_fetch_assoc($result))
+{
+    echo '<h2>'.$record['title'].'</h2>';
+    echo '<p>'.$record['content'].'</p>';
+    echo 'Posted: '.$record['date'];
+    echo '<hr>';
+}
+$connect -> close();
+
+?>
